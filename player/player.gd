52d9522719e64a52ebf41c7438b7167d8f7eb76b
Victor Prazeres <dev.vprazeres@gmail.com>
2026-08-22 20:14:01 -0300

    feat(anim): integrate character animations via code
    Connect character state logic to the animation controller to trigger
    animations programmatically during gameplay.

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var _movement_velocity: float = 128.0
+@export var _player_animator: AnimationPlayer
+
+var _animation_suffix: String = "_down"
 
 func _physics_process(_delta: float) -> void:
 	var direcao = Input.get_vector(
@@ -9,3 +12,32 @@ func _physics_process(_delta: float) -> void:
 	
 	velocity = direcao * _movement_velocity
 	move_and_slide()
+	
+	_animation_suffix = _character_suffix()
+	_animate()
+
+
+func _character_suffix() -> String:
+	var _horizontal_input: float = Input.get_axis("move_left", "move_right")
+	
+	if _horizontal_input == -1:
+		return "_left"
+	
+	if _horizontal_input == +1:
+		return "_right"
+	
+	var _vertical_input: float = Input.get_axis("move_up", "move_down")
+	
+	if _vertical_input == -1:
+		return "_up"
+	
+	if _vertical_input == +1:
+		return "_down"
+	return _animation_suffix
+
+
+func _animate():
+	if velocity:
+		_player_animator.play("walking" + _animation_suffix)
+		return
+	_player_animator.play("idle" + _animation_suffix)
