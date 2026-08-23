002a83a4bd0d6cb8aad0769de9bd2ae5c2f7352f
Victor Prazeres <dev.vprazeres@gmail.com>
2026-08-22 23:03:01 -0300

    feat(player): add sword attack action
    Configure input bindings and combat logic to allow the player to perform
    a sword attack action.

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 @export var _movement_velocity: float = 128.0
 @export var _player_animator: AnimationPlayer
+@export var _action_timer: Timer
 
+var _can_attack: bool = true
 var _animation_suffix: String = "_down"
 
 func _physics_process(_delta: float) -> void:
@@ -14,6 +16,7 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	
 	_animation_suffix = _character_suffix()
+	_attack()
 	_animate()
 
 
@@ -36,8 +39,23 @@ func _character_suffix() -> String:
 	return _animation_suffix
 
 
-func _animate():
+func _attack() -> void:
+	if Input.is_action_just_pressed("attack") and _can_attack:
+		_player_animator.play("attack" + _animation_suffix)
+		_action_timer.start(0.4)
+		_can_attack = false
+		set_physics_process(false)
+
+
+func _animate() -> void:
+	if _can_attack == false:
+		return
 	if velocity:
 		_player_animator.play("walking" + _animation_suffix)
 		return
 	_player_animator.play("idle" + _animation_suffix)
+
+
+func _on_action_timer_timeout() -> void:
+	set_physics_process(true)
+	_can_attack = true
